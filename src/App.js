b257cd817435c17cf6a23cfb9b19963ae4a9f0af
satu1kaa <satu1kaa@gmail.com>
2023-06-27 10:50:05 +0700

    several changes, add components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
+// import Counter from './components/Counter'
+// import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('Text in input')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ])
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
+  ])
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <h2>{value}</h2>
-      <input 
-        type="text" 
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
+      <PostList posts={posts} title="Посты про JS" />
+      <PostList posts={posts2} title="Посты про Python" />
     </div>
   );
 }
 
 export default App;
+
