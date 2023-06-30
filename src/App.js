a56620e79dbc5732047a36c07574ba2c35ced8ba
satu1kaa <satu1kaa@gmail.com>
2023-06-30 12:24:54 +0700

    half way point

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-// import Counter from './components/Counter'
-// import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm'
+import MySelect from './components/UI/select/MySelect';
+
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,16 +11,48 @@ function App() {
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ])
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  // Получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
-      <PostList posts={posts} title="Посты про JS" />
-      <PostList posts={posts2} title="Посты про Python" />
+      <PostForm create={createPost} />
+      <hr style={{ margin: '15px 0px' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка по'
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По содержанию' },
+          ]}
+
+        />
+      </div>
+      {posts.length
+        ?
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+        :
+        <h2 style={{ textAlign: 'center' }}>
+          Посты не найдены!
+        </h2>
+      }
+
     </div>
   );
 }
